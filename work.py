6a78e75658a8664d334fe565ce3df4c0090848cd
Max <46403438+Skotmak@users.noreply.github.com>
2022-01-08 05:49:13 +0300

    create load elements in tab 2 and 3
    I need to finish load elements from tab 4 to tab 2 and 3

--- work.py
+++ work.py
@@ -1,6 +1,6 @@
 from main import Gui
 from window_work import *
-from PyQt5.QtWidgets import QTableWidgetItem 
+from PyQt5.QtWidgets import QTableWidgetItem
 
 class WorkGui(Gui):
     def __init__(self, parent=None):
@@ -9,20 +9,57 @@ class WorkGui(Gui):
         self.ui.setupUi(self)
         self.centerOnScreen()
         self.db = Gui.client.timetable
+        self.db_stud = Gui.client.stuff
         self.coll = self.db.ussual
         self.coll_temp = self.db.temp
+        self.coll_stud = self.db_stud.students
         self.current_id = 0
         #self.current_id_temp = 0
         self.n = 0
         self.ui.plainTextEdit.setReadOnly(True)
 
-        self.n_notes = self.coll_temp.find().count()
-        self.ui.TW_temp.setRowCount(self.n_notes)
+        # Ставит дату по умолчанию на текущую дату
+        self.today = QtCore.QDate.currentDate()
+        self.ui.DE_temp.setDate(self.today)
+
+
+        ''' Tab 2 '''
+        # Действия при инициализации второй вкладки
+        # подсчёт записей во второй вкладке
+        self.count_temp = self.coll_temp.find().count()
+        self.ui.TW_temp.setRowCount(self.count_temp)
+
+        # Добавление заголовков
+        self.ui.TW_temp.horizontalHeader().setVisible(True)
+        self.ui.TW_temp.verticalHeader().setVisible(True)
+
+        # Загрузка списка учащихся
+        self.download_tab(tab=2)
+        
+
+        ''' Tab 3 '''
+        # Действия при инициализации третьей вкладки
+        # подсчёт записей в третьей вкладке
+        self.count_stud = self.coll_stud.find().count()
+        self.ui.stud_tab.setRowCount(self.count_stud)
+
+        # Добавление заголовков
+        self.ui.stud_tab.horizontalHeader().setVisible(True)
+        self.ui.stud_tab.verticalHeader().setVisible(True)
+        
+        # Загрузка списка учащихся
+        self.download_tab(tab=3)
+
+        ''' Tab 4 '''
+        self.download_tab(tab=4)
+        
+
+        ''' End tab '''
 
 
         #self.ui.TW_temp.horizontalHeader().setSectionResizeMode(3, QHeaderView.Stretch)
         #self.ui.TW_temp.horizontalHeader().setSectionResizeMode(4, QHeaderView.ResizeToContents)
-# обнови дизайн документ там ошибка в заголовках
+        # обнови дизайн документ там ошибка в заголовках
         #self.ui.TW_temp.sizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents) проблема в том что не могу поменять ширину столбцов. попоробуй через дизайнер поменять этот параметр
 
         ''' TAB 1 '''
@@ -52,6 +89,55 @@ class WorkGui(Gui):
         self.ui.send_chages_temp.clicked.connect(self.send_temp)
         self.ui.clear_PTE_temp.clicked.connect(lambda tab_num: self.clear_button(tab_num=2))
         self.ui.delete_temp_note.clicked.connect(self.delete_temp)
+
+        ''' End tab '''
+
+
+    def download_tab(self, tab):
+        if tab == 3:
+            self.n_stud = 0
+            for n_stud in range(0, self.count_stud):
+                res_stud = self.coll_stud.find_one({"_id": n_stud})
+                self.l_name = res_stud["l_name"]
+                self.f_name = res_stud["f_name"]
+                self.m_name = res_stud["m_name"]
+                self.number = res_stud["number"]
+                self.ui.stud_tab.setItem(self.n_stud, 0, QTableWidgetItem(self.l_name))
+                self.ui.stud_tab.setItem(self.n_stud, 1,  QTableWidgetItem(self.f_name))
+                self.ui.stud_tab.setItem(self.n_stud, 2, QTableWidgetItem(self.m_name))
+                self.ui.stud_tab.setItem(self.n_stud, 3,  QTableWidgetItem(self.number))
+                self.n_stud += 1
+        elif tab == 2:
+            self.n_temp = 0
+            for n_temp in range(0, self.count_temp):
+                res_temp = self.coll_temp.find_one({"_id": n_temp})
+                self.date_temp = res_temp["date"]
+                self.text_temp = res_temp["text"]
+                self.ui.TW_temp.setItem(self.n_temp, 0, QTableWidgetItem(self.date_temp))
+                self.ui.TW_temp.setItem(self.n_temp, 1,  QTableWidgetItem(self.text_temp))
+                self.n_temp += 1
+        elif tab == 4: # (РАБОТАЕТ!!!! ДОДЕЛАЙ ДЛЯ 3 И 2 ВКЛАДКИ!!!)
+            '''
+            item_1 = QtWidgets.QTreeWidgetItem(self.ui.treeWidget)
+            self.ui.treeWidget.addTopLevelItem(item_1)
+            self.ui.treeWidget.topLevelItem(3).setText(3, "hi")
+            '''
+            self.n_stud = 0
+            for n_stud in range(0, self.count_stud):
+                item_1 = QtWidgets.QTreeWidgetItem(self.ui.treeWidget)
+                self.ui.treeWidget.addTopLevelItem(item_1)
+                res_stud = self.coll_stud.find_one({"_id": n_stud})
+                self.l_name = res_stud["l_name"]
+                self.f_name = res_stud["f_name"]
+                self.m_name = res_stud["m_name"]
+                self.number = res_stud["number"]
+                self.ui.treeWidget.topLevelItem(n_stud).setText(0, self.l_name)
+                self.ui.treeWidget.topLevelItem(n_stud).setText(1, self.f_name)
+                self.ui.treeWidget.topLevelItem(n_stud).setText(2, self.m_name)
+                self.ui.treeWidget.topLevelItem(n_stud).setText(3, self.number)
+                self.n_stud += 1
+
+        return
 
 
     def keyPressEvent(self, event: QtGui.QKeyEvent):
